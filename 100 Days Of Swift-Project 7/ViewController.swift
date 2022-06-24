a2f93f961e561b79f67be3c9c4daec110411c5d1
Arda Büyükhatipoğlu <ardabho@gmail.com>
2022-06-24 15:25:23 +0300

    Day 33 done.

--- 100 Days Of Swift-Project 7/ViewController.swift
+++ 100 Days Of Swift-Project 7/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var petitions = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlLink = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlLink){
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            }
+        }
+        
     }
-
-
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let decodedData = try? decoder.decode(Petitions.self, from: json) {
+            petitions = decodedData.results
+            tableView.reloadData()
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
+        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.detailTextLabel?.text = petitions[indexPath.row].body
+        return cell
+    }
+    
 }
 
